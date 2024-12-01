d21f11d46d38f605d4e19c0d3570d439865e4ee1
WBI\nxg08092 <alice.ju@nxp.com>
2024-12-02 03:42:51 +0900

    feat: tls-server demo

---
+++ src/demo/tls-server/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "model/secure_element.h"
+
+int main(void)
+{
+    int err = SecureElement_init();
+    if (err) {
+        printf("SecureElement_init() failed with error %d\n", err);
+        exit(1);
+    }
+
+    return 0;
+}
---
+++ src/model/secure_element.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int SecureElement_init(void);
